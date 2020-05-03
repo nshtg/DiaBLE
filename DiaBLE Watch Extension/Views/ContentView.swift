2614b340528d38302ae9a9464630bc02c8728601
Guido <gui_dos@libero.it>
2020-05-03 10:57:41 +0200

    Watch app: copy & paste & comment-out & squeeze the DataView

--- DiaBLE Watch Extension/Views/ContentView.swift
+++ DiaBLE Watch Extension/Views/ContentView.swift
@@ -17,38 +17,57 @@ struct ContentView: View {
     @EnvironmentObject var settings: Settings
 
     var body: some View {
-        VStack(spacing: 10) {
-            HStack(spacing: 10) {
-                NavigationLink(destination: Monitor().environmentObject(app).environmentObject(history).environmentObject(settings)) {
-                    VStack {
-                        Image(systemName: "gauge").resizable().frame(width: 40, height: 40).offset(y: 4)
-                        Text("Monitor").bold().foregroundColor(.blue)
-                    }.frame(maxWidth: .infinity).padding(.vertical, 6)
+        ScrollView {
+            VStack(spacing: 10) {
+                HStack(spacing: 10) {
+                    NavigationLink(destination: Monitor().environmentObject(app).environmentObject(history).environmentObject(settings)) {
+                        VStack {
+                            Image(systemName: "gauge").resizable().frame(width: 40, height: 40).offset(y: 4)
+                            Text("Monitor").bold().foregroundColor(.blue)
+                        }.frame(maxWidth: .infinity).padding(.vertical, 6)
+                    }
+                    NavigationLink(destination: Details().environmentObject(app).environmentObject(settings)) {
+                        VStack {
+                            Image(systemName: "info.circle").resizable().frame(width: 40, height: 40).offset(y: 4)
+                            Text("Details").bold().foregroundColor(.blue).bold()
+                        }.frame(maxWidth: .infinity).padding(.vertical, 6)
+                    }
                 }
-                NavigationLink(destination: Details().environmentObject(app).environmentObject(settings)) {
-                    VStack {
-                        Image(systemName: "info.circle").resizable().frame(width: 40, height: 40).offset(y: 4)
-                        Text("Details").bold().foregroundColor(.blue).bold()
-                    }.frame(maxWidth: .infinity).padding(.vertical, 6)
+                HStack(spacing: 10) {
+                    NavigationLink(destination: LogView().environmentObject(app).environmentObject(log).environmentObject(settings)) {
+                        VStack {
+                            Image(systemName: "doc.plaintext").resizable().frame(width: 40, height: 40).offset(y: 4)
+                            Text("Log").bold().foregroundColor(.blue)
+                        }.frame(maxWidth: .infinity).padding(.vertical, 6)
+                    }
+                    NavigationLink(destination: SettingsView().environmentObject(app).environmentObject(log).environmentObject(history).environmentObject(settings)) {
+                        VStack {
+                            Image(systemName: "gear").resizable().frame(width: 40, height: 40).offset(y: 4)
+                            Text("Settings").bold().tracking(-0.5).foregroundColor(.blue)
+                        }.frame(maxWidth: .infinity).padding(.vertical, 6)
+                    }
+                }
+                HStack(spacing: 10) {
+                    // TODO
+                    NavigationLink(destination: DataView().environmentObject(app).environmentObject(log).environmentObject(history).environmentObject(settings)) {
+                        VStack {
+                            Image(systemName: "tray.full.fill").resizable().frame(width: 40, height: 40).offset(y: 4)
+                            Text("Data").bold().foregroundColor(.blue)
+                        }.frame(maxWidth: .infinity).padding(.vertical, 6)
+                    }
+                    NavigationLink(destination: Monitor().environmentObject(app).environmentObject(history).environmentObject(log).environmentObject(settings)) {
+                        VStack {
+                            Image(systemName: "globe").resizable().frame(width: 40, height: 40).offset(y: 4)
+                            Text("Online").bold().foregroundColor(.blue)
+                        }.frame(maxWidth: .infinity).padding(.vertical, 6)
+                    }
                 }
             }
-            HStack(spacing: 10) {
-                NavigationLink(destination: LogView().environmentObject(app).environmentObject(log).environmentObject(settings)) {
-                    VStack {
-                        Image(systemName: "doc.plaintext").resizable().frame(width: 40, height: 40).offset(y: 4)
-                        Text("Log").bold().foregroundColor(.blue)
-                    }.frame(maxWidth: .infinity).padding(.vertical, 6)
-                }
-                NavigationLink(destination: SettingsView().environmentObject(app).environmentObject(log).environmentObject(history).environmentObject(settings)) {
-                    VStack {
-                        Image(systemName: "gear").resizable().frame(width: 40, height: 40).offset(y: 4)
-                        Text("Settings").bold().tracking(-0.5).foregroundColor(.blue)
-                    }.frame(maxWidth: .infinity).padding(.vertical, 6)
-                }
-            }
-        }.padding(4)
-            .foregroundColor(.red)
-            .buttonStyle(PlainButtonStyle())
+        }
+        .navigationBarTitle("DiaBLE  \(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String)")
+        .edgesIgnoringSafeArea([.bottom])
+        .foregroundColor(.red)
+        .buttonStyle(PlainButtonStyle())
     }
 }
 
---
+++ DiaBLE Watch Extension/Views/DataView.swift
@@ -0,0 +1,146 @@
+import Foundation
+import SwiftUI
+
+
+struct DataView: View {
+    @EnvironmentObject var app: App
+    @EnvironmentObject var history: History
+    @EnvironmentObject var settings: Settings
+
+    @State private var readingCountdown: Int = 0
+
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+
+    var body: some View {
+        VStack {
+
+            Text("\(Date().dateTime)")
+                .foregroundColor(.white)
+
+            if app.deviceState == "Connected" {
+                Text(readingCountdown > 0 || app.info.hasSuffix("sensor") ?
+                    "\(readingCountdown) s" : "")
+                    .fixedSize()
+                    .onReceive(timer) { _ in
+                        self.readingCountdown = self.settings.readingInterval * 60 - Int(Date().timeIntervalSince(self.app.lastReadingDate))
+                }.font(Font.caption.monospacedDigit()).foregroundColor(.orange)
+            }
+
+            VStack {
+                HStack {
+                    if history.values.count > 0 {
+                        VStack(spacing: 4) {
+                            Text("OOP history")
+                            ScrollView {
+                                ForEach(history.values) { glucose in
+                                    Text("\(glucose.id) \(glucose.date.shortDateTime)  \(Int(glucose.value), specifier: "%3lld")")
+                                }
+                            }.frame(maxWidth: .infinity, alignment: .topLeading)
+                        }.foregroundColor(.blue)
+                    }
+
+                    if history.rawValues.count > 0 {
+                        VStack(spacing: 4) {
+                            Text("Raw history")
+                            ScrollView {
+                                ForEach(history.rawValues) { glucose in
+                                    Text("\(glucose.id) \(glucose.date.shortDateTime)  \(Int(glucose.value), specifier: "%3lld")")
+                                }
+                            }.frame(maxWidth: .infinity, alignment: .topLeading)
+                        }.foregroundColor(.yellow)
+                    }
+                }
+
+                HStack {
+                    if history.calibratedValues.count > 0 {
+                        VStack(spacing: 4) {
+                            Text("Calibrated history")
+                            ScrollView {
+                                ForEach(history.calibratedValues) { glucose in
+                                    Text("\(glucose.id) \(glucose.date.shortDateTime)  \(Int(glucose.value), specifier: "%3lld")")
+                                }
+                            }.frame(maxWidth: .infinity, alignment: .topLeading)
+                        }.foregroundColor(.purple)
+                    }
+
+                    VStack {
+
+                        if history.rawTrend.count > 0 {
+                            VStack(spacing: 4) {
+                                Text("Raw trend")
+                                ScrollView {
+                                    ForEach(history.rawTrend) { glucose in
+                                        Text("\(glucose.id) \(glucose.date.shortDateTime)  \(Int(glucose.value), specifier: "%3lld")")
+                                    }
+                                }.frame(maxWidth: .infinity, alignment: .topLeading)
+                            }.foregroundColor(.yellow)
+                        }
+
+                        if history.calibratedTrend.count > 0 {
+                            VStack(spacing: 4) {
+                                Text("Calibrated trend")
+                                ScrollView {
+                                    ForEach(history.calibratedTrend) { glucose in
+                                        Text("\(glucose.id) \(glucose.date.shortDateTime)  \(Int(glucose.value), specifier: "%3lld")")
+                                    }
+                                }.frame(maxWidth: .infinity, alignment: .topLeading)
+                            }.foregroundColor(.purple)
+                        }
+                    }
+                }
+
+                HStack(spacing: 0) {
+                    if history.storedValues.count > 0 {
+                        VStack(spacing: 0) {
+                            Text("HealthKit")
+                            List() {
+                                ForEach(history.storedValues) { glucose in
+                                    Text("\(String(glucose.source[..<(glucose.source.lastIndex(of: " ") ?? glucose.source.endIndex)])) \(glucose.date.shortDateTime)  \(Int(glucose.value), specifier: "%3lld")")
+                                        .fixedSize(horizontal: false, vertical: true).listRowInsets(EdgeInsets()).listRowInsets(EdgeInsets())
+                                }
+                            }
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                        }.foregroundColor(.red)
+                            .onAppear { if let healthKit = self.app.main?.healthKit { healthKit.read() } }
+                    }
+
+                    if history.nightscoutValues.count > 0 {
+                        VStack(spacing: 0) {
+                            Text("Nightscout")
+                            List() {
+                                ForEach(history.nightscoutValues) { glucose in
+                                    Text("\(String(glucose.source[..<(glucose.source.lastIndex(of: " ") ?? glucose.source.endIndex)])) \(glucose.date.shortDateTime)  \(Int(glucose.value), specifier: "%3lld")")
+                                        .fixedSize(horizontal: false, vertical: true).listRowInsets(EdgeInsets())
+                                }
+                                .frame(maxWidth: .infinity, alignment: .topLeading)
+                            }
+                        }.foregroundColor(.blue)
+                        // .onAppear { if let nightscout = self.app.main?.nightscout { nightscout.read() } }
+                    }
+                }
+            }
+        }
+        .navigationBarHidden(true)
+        .edgesIgnoringSafeArea([.bottom])
+            // .font(.system(.footnote, design: .monospaced)).foregroundColor(Color(UIColor.lightGray))
+            .font(.footnote).foregroundColor(Color(UIColor.lightGray))
+            .navigationBarTitle("Data")
+    }
+}
+
+
+struct DataView_Previews: PreviewProvider {
+    @EnvironmentObject var app: App
+    @EnvironmentObject var history: History
+    @EnvironmentObject var settings: Settings
+    static var previews: some View {
+        Group {
+            DataView()
+                .environmentObject(App.test(tab: .data))
+                .environmentObject(History.test)
+                .environmentObject(Settings())
+                .environment(\.colorScheme, .dark)
+        }
+    }
+}
